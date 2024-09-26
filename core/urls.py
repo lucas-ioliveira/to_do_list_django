9525e19c1948de33ca0e2fbf757a42ac68ba7806
Lucas Oliveira <lucasio2008@gmail.com>
2024-09-26 19:54:22 -0300

    feat: Desenvolvido nova rota.

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 # Home
 from .views import IndexView
 # Funcionalidades do sistema
-from .views import TarefasView, CadastrarTarefaView, AtualizarTarefaView, DeletarTarefaView, TarefasConcluidasView, TarefaConcluirView
+from .views import TarefasView, CadastrarTarefaView, AtualizarTarefaView, DeletarTarefaView, TarefasConcluidasView, TarefaConcluirView, TarefasAndamentoView
 
 app_name = 'tasks'
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
     # Urls funcionalidades do sistema
     path('tarefas/', TarefasView.as_view(), name='tarefas'),
     path('tarefas/concluidas', TarefasConcluidasView.as_view(), name='tarefas_concluidas'),
+    path('tarefas/andamento', TarefasAndamentoView.as_view(), name='tarefas_andamento'),
     path('tarefas/add/', CadastrarTarefaView.as_view(), name='add'),
     path('tarefas/<int:pk>/atualizar', AtualizarTarefaView.as_view(), name='tarefa_atl'),
     path('tarefas/<int:pk>/deletar', DeletarTarefaView.as_view(), name='tarefa_del'),
