48da54f8abb8e2b87331951107576b48e1b92ccc
Lucas Oliveira <lucasio2008@gmail.com>
2025-02-01 20:07:56 -0300

    feat: Add wwork space ao filtro.

--- core/utils.py
+++ core/utils.py
@@ -2,6 +2,6 @@ from .models import ToDoList
 from datetime import datetime, timedelta
 
 
-def get_tarefas_by_status(user, status, days=30):
+def get_tarefas_by_status(user, status, days=30, work_space=None):
     data_limite = datetime.now() - timedelta(days=days)
-    return ToDoList.objects.filter(usuario=user, status=status, data_criacao__gte=data_limite)
+    return ToDoList.objects.filter(usuario=user, status=status, data_criacao__gte=data_limite, work_space__id=work_space)
