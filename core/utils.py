dcb15e0d97efcea22fce146623bf75ab4c91a5a5
Lucas Oliveira <lucasio2008@gmail.com>
2025-02-02 13:31:50 -0300

    feat: Add chave ao filtro.

--- core/utils.py
+++ core/utils.py
@@ -4,4 +4,4 @@ from datetime import datetime, timedelta
 
 def get_tarefas_by_status(user, status, days=30, work_space=None):
     data_limite = datetime.now() - timedelta(days=days)
-    return ToDoList.objects.filter(usuario=user, status=status, data_criacao__gte=data_limite, work_space__id=work_space)
+    return ToDoList.objects.filter(usuario=user, status=status, data_criacao__gte=data_limite, work_space__id=work_space, ativo=True).order_by('-data_criacao')
