d069ee17ad7d48571dedf1c4ad26d1e6316571b4
Lucas Oliveira <lucasio2008@gmail.com>
2025-02-04 16:39:51 -0300

    feat: Add implementação de logs para o sistema.

--- todolist/settings.py
+++ todolist/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Libs
+    'easyaudit',
     'bootstrap4',
     # My apps
     'core',
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'easyaudit.middleware.easyaudit.EasyAuditMiddleware',
     
 ]
 
